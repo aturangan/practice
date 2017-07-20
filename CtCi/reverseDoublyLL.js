ec2584dd8650b96e7c78867db7141031fc49665a
Alana <alana.turangan@gmail.com>
2017-07-19 18:32:21 -0700

    Made Edits

--- CtCi/reverseDoublyLL.js
+++ CtCi/reverseDoublyLL.js
@@ -1,1 +1,42 @@
-ECHO is on.
+let node = {
+  value: val,
+  next: null,
+  previous: null  
+}; 
+
+function DoublyLinkedList(){
+  this.head = null;
+}
+
+DoublyLinkedList.prototype.push = function(val){
+  var head = this.head,
+    current = head,
+    previous = head;
+  
+  if(!head){
+    this.head = {value: val, previous:null, next:null };
+  } else {
+    while(current && current.next){
+      previous = current;
+      current = current.next;
+    }     
+    current.next = {value: val, previous:current, next:null}
+  }  
+}
+
+function reverseDoublyLL(dll){
+   var head = dll.head,
+       current = dll.head,
+       tmp;
+   while(current){
+      tmp = current.next;
+      current.next = current.previous;
+      current.previous = tmp;
+      if(!tmp){
+         //set the last node as header
+         dll.head = current;
+      }
+      current = tmp;
+   }
+  return dll;
+}
--- Interview/highestOf3.js
+++ Interview/highestOf3.js
@@ -1,3 +1,48 @@
 //Given an array of integers, find the highest product you can get from three of the integers.
 //The input arrayOfInts will always have at least three integers.
 
+let highestProduct = (integers) => {
+  let highest = Math.max(integers[0], integers[1]); 
+  let lowest = Math.max(integers[0], integers[1]); 
+  
+  let highestProductOf2 = integers[0] * integers[1]; 
+  let lowestProductOf2 = integers[0] * integers[1]; 
+  
+  let final3 = integers[0] * integers[1] * integers[2]; 
+  
+  for (let i = 0; i < integers.length; i++) {
+    let current = integers[i]; 
+    
+    final3 = Math.max(
+      final3, 
+      current * highestProductOf2, 
+      current * lowestProductOf2
+    ); 
+    
+    highestProductOf2 = Math.max(
+      highestProductOf2, 
+      current * highest, 
+      current * lowest
+    );
+    
+    lowestProductOf2 = Math.min(
+      lowestProductOf2, 
+      current * highest, 
+      current * lowest
+    )
+    
+    highest = Math.max(
+      highest, 
+      current
+    ); 
+    
+    lowest = Math.min(
+      lowest, 
+      current
+    ); 
+  }
+  
+  return final3; 
+};
+
+highestProduct([-10,-10,1,3,2]);
