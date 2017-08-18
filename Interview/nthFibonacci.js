606a6ae48a167ff0184c4b70e7a2bc8254eeb5b6
Alana <alana.turangan@gmail.com>
2017-08-18 14:54:12 -0700

    Iterative and Recursive Solution

---
+++ Interview/nthFibonacci.js
@@ -0,0 +1,36 @@
+//given a function fib, return the nth fibonacci number 
+
+//iterative solution, O(n) time & O(1) space
+let fib = (n) => {
+  if (n < 0) {
+    throw new Error('That number is negative'); 
+  } else if (n === 0) {
+    return 0; 
+  } else if (n === 1) {
+    return 1; 
+  }
+  
+  let prevprev = 0; 
+  let prev = 1; 
+  let current; 
+  
+  for (let i = 1; i < n; i++) {
+    current = prev + prevprev; 
+    prevprev = prev; 
+    prev = current; 
+  }
+  
+  return current; 
+};
+
+//recursive solution 
+
+let fib = (n) => {
+  if (n === 0) {
+    return 0; 
+  } else if (n === 1) {
+    return 1; 
+  }
+
+  return fib(n - 1) + fib(n - 2); 
+};
